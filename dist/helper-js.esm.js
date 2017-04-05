a9e5cb948c1c7e7ffbb3583c85f06bb54f78a8fa
phphe <phphe@outlook.com>
2017-04-05 20:49:43 +0800

    0

---
+++ dist/helper-js.esm.js
@@ -0,0 +1,346 @@
+/*!
+ * helper-js v1.0.0
+ * phphe <phphe@outlook.com> (https://github.com/phphe)
+ * undefined
+ * Released under the MIT License.
+ */
+
+// is 各种判断
+function isset(v) {
+  return typeof v !== 'undefined';
+}
+function isArray(v) {
+  return Object.prototype.toString.call(v) === '[object Array]';
+}
+function isBool(v) {
+  return Object.prototype.toString.call(v) === '[object Boolean]';
+}
+function isNumber(v) {
+  return Object.prototype.toString.call(v) === '[object Number]';
+}
+function isNumeric(v) {
+  var num = parseFloat(v);
+  return !isNaN(num) && isNumber(num);
+}
+function isString(v) {
+  return Object.prototype.toString.call(v) === '[object String]';
+}
+function isObject(v) {
+  return Object.prototype.toString.call(v) === '[object Object]';
+}
+function isFunction(v) {
+  return typeof v === 'function';
+}
+function isPromise(v) {
+  return Object.prototype.toString.call(v) === '[object Promise]';
+}
+function empty(v) {
+  if (v == null) {
+    return true;
+  } else if (v.length != null) {
+    return v.length === 0;
+  } else if (isBool(v)) {
+    return false;
+  } else if (isNumber(v)) {
+    return isNaN(v);
+  } else if (isObject(v)) {
+    return Object.keys(v).length === 0;
+  }
+}
+// num
+function numRand(min, max) {
+  if (arguments.length === 1) {
+    max = min;
+    min = 0;
+  }
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+function numPad(num, n) {
+  var len = num.toString().length;
+  while (len < n) {
+    num = '0' + num;
+    len++;
+  }
+  return num;
+}
+// str 字符
+function studlyCase(str) {
+  return str && str[0].toUpperCase() + str.substr(1);
+}
+function camelCase(str) {
+  var temp = str.toString().split(/[-_]/);
+  for (var i = 1; i < temp.length; i++) {
+    temp[i] = studlyCase(temp[i]);
+  }
+  return temp.join('');
+}
+function camelToWords(str) {
+  return str.toString().trim().split(/(?=[A-Z])/);
+}
+function titleCase(str) {
+  return camelToWords(studlyCase(camelCase(str))).join(' ').replace(/\bid\b/ig, 'ID');
+}
+function strRand() {
+  var len = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 8;
+  var prefix = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : '';
+
+  var r = '';
+  var seeds = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (var i = 0; i < len; i++) {
+    r += seeds[numRand(seeds.length - 1)];
+  }
+  return prefix + r;
+}
+function replaceMultiple(mapObj, str) {
+  var reg = new RegExp(Object.keys(mapObj).join('|'), 'g');
+  return str.replace(reg, function (matchedKey) {
+    return mapObj[matchedKey];
+  });
+}
+// array
+function arrayRemove(arr, v) {
+  var index = arr.indexOf(v);
+  if (index > -1) {
+    arr.splice(index, 1);
+  }
+  return arr;
+}
+function arrayFirst(arr) {
+  return arr[0];
+}
+function arrayLast(arr) {
+  return arr[arr.length - 1];
+}
+function arrayDiff(arr1, arr2) {
+  var len = arr1.length;
+  var arr = [];
+  while (len--) {
+    if (arr2.indexOf(arr1[len]) < 0) {
+      arr.push(arr1[len]);
+    }
+  }
+  return arr;
+}
+
+// object
+function assignIfDifferent(obj, key, val) {
+  if (obj[key] !== val) {
+    obj[key] = val;
+  }
+}
+function objectMerge(o1, o2) {
+  for (var k in o2) {
+    if (!o1.hasOwnProperty(k)) {
+      o1[k] = o2[k];
+    } else if (isObject(o1[k]) && isObject(o2[k])) {
+      Object.assign(o1[k], o2[k]);
+    } else {
+      o1[k] = o2[k];
+    }
+  }
+  return o1;
+}
+function objectMap(obj, func) {
+  var r = {};
+  for (var key in obj) {
+    r[key] = func(obj[key], key, obj);
+  }
+  return r;
+}
+function objectOnly(obj, keys) {
+  var r = {};
+  for (var key in obj) {
+    if (keys.indexOf(key) > -1) {
+      r[key] = obj[key];
+    }
+  }
+  return r;
+}
+function objectExcept(obj, keys) {
+  var r = {};
+  for (var key in obj) {
+    if (keys.indexOf(key) === -1) {
+      r[key] = obj[key];
+    }
+  }
+  return r;
+}
+
+// url
+/* eslint-disable */
+function getUrlParam(par) {
+  // 获取当前URL
+  var local_url = document.location.href;
+  // 获取要取得的get参数位置
+  var get = local_url.indexOf(par + '=');
+  if (get == -1) {
+    return false;
+  }
+  // 截取字符串
+  var get_par = local_url.slice(par.length + get + 1);
+  // 判断截取后的字符串是否还有其他get参数
+  var nextPar = get_par.indexOf('&');
+  if (nextPar != -1) {
+    get_par = get_par.slice(0, nextPar);
+  }
+  return get_par;
+}
+/* eslint-enable */
+// dom
+var _generatedIds = [];
+function uniqueId() {
+  var prefix = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'id_';
+
+  var id = prefix + strRand();
+  if (document.getElementById(id) || _generatedIds.includes(id)) {
+    return uniqueId(prefix);
+  } else {
+    _generatedIds.push(id);
+    return id;
+  }
+}
+function isDescendantOf(el, parent) {
+  while (true) {
+    if (el.parentElement == null) {
+      return false;
+    } else if (el.parentElement === parent) {
+      return true;
+    } else {
+      el = el.parentElement;
+    }
+  }
+}
+function getOffset(el) {
+  var elOffset = {
+    x: el.offsetLeft,
+    y: el.offsetTop
+  };
+  var parentOffset = { x: 0, y: 0 };
+  if (el.offsetParent != null) parentOffset = getOffset(el.offsetParent);
+  return {
+    x: elOffset.x + parentOffset.x,
+    y: elOffset.y + parentOffset.y
+  };
+}
+function findParent(el, callback) {
+  if (el.parentElement) {
+    if (callback(el.parentElement)) {
+      return el.parentElement;
+    } else {
+      return findParent(el.parentElement, callback);
+    }
+  }
+}
+function hasClass(el, className) {
+  if (el.classList) {
+    return el.classList.contains(className);
+  } else {
+    return new RegExp('(^| )' + className + '( |$)', 'gi').test(el.className);
+  }
+}
+/**
+ * [isOffsetInEl]
+ * @param {Number} x
+ * @param {Number} y
+ * @param {Object} el HTML Element
+ */
+function isOffsetInEl(x, y, el) {
+  var offset = getOffset(el);
+  return offset.x <= x && offset.x + el.offsetWidth >= x && offset.y <= y && offset.y + el.offsetHeight >= y;
+}
+// get border
+function getBorder(el) {
+  var body = document.body;
+  var workArea = findParent(el, function (v) {
+    return hasClass(v, 'work-area');
+  });
+  var of = getOffset(workArea);
+  return {
+    left: of.x,
+    right: of.x + workArea.offsetWidth,
+    top: of.y + 50,
+    bottom: body.offsetHeight < window.innerHeight ? window.innerHeight : body.offsetHeight
+  };
+}
+// advance
+// binarySearch 二分查找
+function binarySearch(arr, callback) {
+  var max = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 1000;
+
+  var midNum;
+  var mid;
+  var start = 0;
+  var end = arr.length - 1;
+  var i = 0;
+  while (start >= 0 && start <= end) {
+    if (i >= max) {
+      throw Error('binarySearch: loop times is over ' + max + ', you can increase the limit.');
+    }
+    midNum = Math.floor((end - start) / 2 + start);
+    mid = arr[midNum];
+    var r = callback(mid, arr, i);
+    if (r < 0) {
+      end = midNum - 1;
+    } else if (r > 0) {
+      start = midNum + 1;
+    } else {
+      return mid;
+    }
+    i++;
+  }
+  return null;
+}
+// overload waitFor(condition, time = 100, maxCount = 1000))
+function waitFor(name, condition) {
+  var time = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 100;
+  var maxCount = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 1000;
+
+  if (isFunction(name)) {
+    maxCount = time;
+    time = isNumeric(condition) ? condition : 100;
+    condition = name;
+    name = null;
+  }
+  if (!waitFor._waits) {
+    waitFor._waits = {};
+  }
+  var waits = waitFor._waits;
+  if (name && isset(waits[name])) {
+    window.clearInterval(waits[name]);
+    delete waits[name];
+  }
+  return new Promise(function (resolve, reject) {
+    var count = 0;
+    function judge(interval) {
+      if (count <= maxCount) {
+        if (condition()) {
+          stop(interval, name);
+          resolve();
+        }
+      } else {
+        stop(interval, name);
+        reject(new Error('waitFor: Limit is reached'));
+      }
+      count++;
+    }
+    function stop(interval, name) {
+      if (interval) {
+        if (name && isset(waits[name])) {
+          window.clearInterval(waits[name]);
+          delete waits[name];
+        } else {
+          window.clearInterval(interval);
+        }
+      }
+    }
+    var interval = window.setInterval(function () {
+      judge(interval);
+    }, time);
+    if (name) {
+      waits[name] = interval;
+    }
+    judge();
+  });
+}
+
+export { isset, isArray, isBool, isNumber, isNumeric, isString, isObject, isFunction, isPromise, empty, numRand, numPad, studlyCase, camelCase, camelToWords, titleCase, strRand, replaceMultiple, arrayRemove, arrayFirst, arrayLast, arrayDiff, assignIfDifferent, objectMerge, objectMap, objectOnly, objectExcept, getUrlParam, uniqueId, isDescendantOf, getOffset, findParent, hasClass, isOffsetInEl, getBorder, binarySearch, waitFor };
---
+++ src/helper-js.js
@@ -0,0 +1,325 @@
+// is 各种判断
+export function isset (v) {
+  return typeof v !== 'undefined'
+}
+export function isArray (v) {
+  return Object.prototype.toString.call(v) === '[object Array]'
+}
+export function isBool (v) {
+  return Object.prototype.toString.call(v) === '[object Boolean]'
+}
+export function isNumber (v) {
+  return Object.prototype.toString.call(v) === '[object Number]'
+}
+export function isNumeric (v) {
+  const num = parseFloat(v)
+  return !isNaN(num) && isNumber(num)
+}
+export function isString (v) {
+  return Object.prototype.toString.call(v) === '[object String]'
+}
+export function isObject (v) {
+  return Object.prototype.toString.call(v) === '[object Object]'
+}
+export function isFunction (v) {
+  return typeof v === 'function'
+}
+export function isPromise (v) {
+  return Object.prototype.toString.call(v) === '[object Promise]'
+}
+export function empty(v) {
+  if (v == null) {
+    return true
+  } else if (v.length != null) {
+    return v.length === 0
+  } else if (isBool(v)) {
+    return false
+  } else if (isNumber(v)) {
+    return isNaN(v)
+  } else if (isObject(v)) {
+    return Object.keys(v).length === 0
+  }
+}
+// num
+export function numRand (min, max) {
+  if (arguments.length === 1) {
+    max = min
+    min = 0
+  }
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+export function numPad (num, n) {
+  let len = num.toString().length
+  while (len < n) {
+    num = '0' + num
+    len++
+  }
+  return num
+}
+// str 字符
+export function studlyCase (str) {
+  return str && (str[0].toUpperCase() + str.substr(1))
+}
+export function camelCase (str) {
+  const temp = str.toString().split(/[-_]/)
+  for (let i = 1; i < temp.length; i++) {
+    temp[i] = studlyCase(temp[i])
+  }
+  return temp.join('')
+}
+export function camelToWords (str) {
+  return str.toString().trim().split(/(?=[A-Z])/)
+}
+export function titleCase (str) {
+  return camelToWords(studlyCase(camelCase(str))).join(' ').replace(/\bid\b/ig, 'ID')
+}
+export function strRand (len = 8, prefix = '') {
+  let r = ''
+  const seeds = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  for (let i = 0; i < len; i++) {
+    r += seeds[numRand(seeds.length - 1)]
+  }
+  return prefix + r
+}
+export function replaceMultiple(mapObj, str) {
+  const reg = new RegExp(Object.keys(mapObj).join('|'), 'g')
+  return str.replace(reg, function (matchedKey) {
+    return mapObj[matchedKey]
+  })
+}
+// array
+export function arrayRemove(arr, v) {
+  const index = arr.indexOf(v)
+  if (index > -1) {
+    arr.splice(index, 1)
+  }
+  return arr
+}
+export function arrayFirst(arr) {
+  return arr[0]
+}
+export function arrayLast(arr) {
+  return arr[arr.length - 1]
+}
+export function arrayDiff(arr1, arr2) {
+  var len = arr1.length
+  var arr = []
+  while (len--) {
+    if (arr2.indexOf(arr1[len]) < 0) {
+      arr.push(arr1[len])
+    }
+  }
+  return arr
+}
+
+// object
+export function assignIfDifferent(obj, key, val) {
+  if (obj[key] !== val) {
+    obj[key] = val
+  }
+}
+export function objectMerge(o1, o2) {
+  for (const k in o2) {
+    if (!o1.hasOwnProperty(k)) {
+      o1[k] = o2[k]
+    } else if (isObject(o1[k]) && isObject(o2[k])) {
+      Object.assign(o1[k], o2[k])
+    } else {
+      o1[k] = o2[k]
+    }
+  }
+  return o1
+}
+export function objectMap(obj, func) {
+  const r = {}
+  for (const key in obj) {
+    r[key] = func(obj[key], key, obj)
+  }
+  return r
+}
+export function objectOnly(obj, keys) {
+  const r = {}
+  for (const key in obj) {
+    if (keys.indexOf(key) > -1) {
+      r[key] = obj[key]
+    }
+  }
+  return r
+}
+export function objectExcept(obj, keys) {
+  const r = {}
+  for (const key in obj) {
+    if (keys.indexOf(key) === -1) {
+      r[key] = obj[key]
+    }
+  }
+  return r
+}
+
+// url
+/* eslint-disable */
+export function getUrlParam(par) {
+    // 获取当前URL
+  var local_url = document.location.href
+    // 获取要取得的get参数位置
+  var get = local_url.indexOf(par + '=')
+  if (get == -1) {
+    return false
+  }
+    // 截取字符串
+  var get_par = local_url.slice(par.length + get + 1)
+    // 判断截取后的字符串是否还有其他get参数
+  var nextPar = get_par.indexOf('&')
+  if (nextPar != -1) {
+    get_par = get_par.slice(0, nextPar)
+  }
+  return get_par
+}
+/* eslint-enable */
+// dom
+const _generatedIds = []
+export function uniqueId (prefix = 'id_') {
+  const id = prefix + strRand()
+  if (document.getElementById(id) || _generatedIds.includes(id)) {
+    return uniqueId(prefix)
+  } else {
+    _generatedIds.push(id)
+    return id
+  }
+}
+export function isDescendantOf (el, parent) {
+  while (true) {
+    if (el.parentElement == null) {
+      return false
+    } else if (el.parentElement === parent) {
+      return true
+    } else {
+      el = el.parentElement
+    }
+  }
+}
+export function getOffset(el) {
+  var elOffset = {
+    x: el.offsetLeft,
+    y: el.offsetTop
+  }
+  var parentOffset = {x: 0, y: 0}
+  if (el.offsetParent != null) parentOffset = getOffset(el.offsetParent)
+  return {
+    x: elOffset.x + parentOffset.x,
+    y: elOffset.y + parentOffset.y
+  }
+}
+export function findParent(el, callback) {
+  if (el.parentElement) {
+    if (callback(el.parentElement)) {
+      return el.parentElement
+    } else {
+      return findParent(el.parentElement, callback)
+    }
+  }
+}
+export function hasClass(el, className) {
+  if (el.classList) {
+    return el.classList.contains(className)
+  } else {
+    return new RegExp('(^| )' + className + '( |$)', 'gi').test(el.className)
+  }
+}
+/**
+ * [isOffsetInEl]
+ * @param {Number} x
+ * @param {Number} y
+ * @param {Object} el HTML Element
+ */
+export function isOffsetInEl(x, y, el) {
+  const offset = getOffset(el)
+  return offset.x <= x && offset.x + el.offsetWidth >= x && offset.y <= y && offset.y + el.offsetHeight >= y
+}
+// get border
+export function getBorder(el) {
+  const body = document.body
+  const workArea = findParent(el, v => hasClass(v, 'work-area'))
+  const of = getOffset(workArea)
+  return {
+    left: of.x,
+    right: of.x + workArea.offsetWidth,
+    top: of.y + 50,
+    bottom: body.offsetHeight < window.innerHeight ? window.innerHeight : body.offsetHeight
+  }
+}
+// advance
+// binarySearch 二分查找
+export function binarySearch(arr, callback, max = 1000) {
+  var midNum
+  var mid
+  var start = 0
+  var end = arr.length - 1
+  var i = 0
+  while (start >= 0 && start <= end) {
+    if (i >= max) {
+      throw Error(`binarySearch: loop times is over ${max}, you can increase the limit.`)
+    }
+    midNum = Math.floor((end - start) / 2 + start)
+    mid = arr[midNum]
+    const r = callback(mid, arr, i)
+    if (r < 0) {
+      end = midNum - 1
+    } else if (r > 0) {
+      start = midNum + 1
+    } else {
+      return mid
+    }
+    i++
+  }
+  return null
+}
+// overload waitFor(condition, time = 100, maxCount = 1000))
+export function waitFor(name, condition, time = 100, maxCount = 1000) {
+  if (isFunction(name)) {
+    maxCount = time
+    time = isNumeric(condition) ? condition : 100
+    condition = name
+    name = null
+  }
+  if (!waitFor._waits) {
+    waitFor._waits = {}
+  }
+  const waits = waitFor._waits
+  if (name && isset(waits[name])) {
+    window.clearInterval(waits[name])
+    delete waits[name]
+  }
+  return new Promise(function(resolve, reject) {
+    let count = 0
+    function judge(interval) {
+      if (count <= maxCount) {
+        if (condition()) {
+          stop(interval, name)
+          resolve()
+        }
+      } else {
+        stop(interval, name)
+        reject(new Error('waitFor: Limit is reached'))
+      }
+      count++
+    }
+    function stop(interval, name) {
+      if (interval) {
+        if (name && isset(waits[name])) {
+          window.clearInterval(waits[name])
+          delete waits[name]
+        } else {
+          window.clearInterval(interval)
+        }
+      }
+    }
+    const interval = window.setInterval(function () {
+      judge(interval)
+    }, time)
+    if (name) {
+      waits[name] = interval
+    }
+    judge()
+  })
+}
