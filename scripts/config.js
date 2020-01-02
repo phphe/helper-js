93e25474d7014372ebf00b2ce889a61a82436e14
phphe <phphe@outlook.com>
2020-01-02 23:42:27 +0800

    1.4.19 beta

--- scripts/config.js
+++ scripts/config.js
@@ -1,14 +1,6 @@
-// From vue. 参考vue
-// https://github.com/vuejs/vue/blob/dev/scripts/config.js
+const {genConfig: genConfig0, camelCase, defaultPlugins} = require('rollup-helper')
 const path = require('path')
 const fs = require('fs')
-const babel = require('rollup-plugin-babel')
-const alias = require('@rollup/plugin-alias')
-const cjs = require('@rollup/plugin-commonjs')
-const replace = require('@rollup/plugin-replace')
-const node = require('@rollup/plugin-node-resolve')
-const json = require('@rollup/plugin-json')
-const {terser} = require('rollup-plugin-terser')
 const pkg = require('../package.json')
 const resolve = p => path.resolve(__dirname, '../', p)
 
@@ -16,6 +8,12 @@ const options = {
   input: fs.existsSync(resolve('src/index.js')) ? resolve(`src/index.js`) : resolve(`src/${pkg.name}.js`),
   outputName: pkg.name,
   moduleName: camelCase(pkg.name),
+  external_cjs_esm: source => {
+    const external = [/^core-js/, /^@babel\/runtime/]
+    if (external.find(re => re === source || re.test(source))) {
+      return true
+    }
+  },
 }
 
 const builds = {
@@ -24,12 +22,14 @@ const builds = {
     dest: resolve(`dist/${options.outputName}.cjs.js`),
     format: 'cjs',
     plugins: defaultPlugins(),
+    external: options.external_cjs_esm,
   },
   'esm': {
     entry: options.input,
     dest: resolve(`dist/${options.outputName}.esm.js`),
     format: 'es',
     plugins: defaultPlugins(),
+    external: options.external_cjs_esm,
   },
   'umd': {
     entry: options.input,
@@ -44,52 +44,13 @@ const builds = {
     format: 'umd',
     plugins: defaultPlugins(),
     moduleName: options.moduleName,
+    sourcemap: true,
   },
 }
 
 const aliases = require('./alias')
-function genConfig (name) {
-  const opts = builds[name]
-  const config = {
-    input: opts.entry,
-    external: opts.external,
-    plugins: [
-      alias(Object.assign({}, aliases, opts.alias)),
-      ...opts.plugins,
-    ],
-    output: {
-      file: opts.dest,
-      format: opts.format,
-      banner: opts.banner || getDefaultBanner(),
-      name: opts.moduleName,
-    },
-    onwarn: (msg, warn) => {
-      if (!/Circular/.test(msg)) {
-        warn(msg)
-      }
-    }
-  }
-
-  // built-in vars
-  const vars = {}
-  // build-specific env
-  if (opts.env) {
-    vars['process.env.NODE_ENV'] = JSON.stringify(opts.env)
-  }
-  if (Object.keys(vars).length > 0) {
-    config.plugins.push(replace(vars))
-  }
-  const isProd = /(min|prod)\.js$/.test(config.output.file)
-  if (isProd) {
-    config.plugins.push(terser())
-  }
-
-  Object.defineProperty(config, '_name', {
-    enumerable: false,
-    value: name
-  })
-
-  return config
+function genConfig(name) {
+  return genConfig0({name, pkg, aliases, builds})
 }
 
 if (process.env.TARGET) {
@@ -97,35 +58,4 @@ if (process.env.TARGET) {
 } else {
   exports.getBuild = genConfig
   exports.getAllBuilds = () => Object.keys(builds).map(genConfig)
-}
-function studlyCase (str) {
-  return str && (str[0].toUpperCase() + str.substr(1))
-}
-function camelCase (str) {
-  const temp = str.toString().split(/[-_]/)
-  for (let i = 1; i < temp.length; i++) {
-    temp[i] = studlyCase(temp[i])
-  }
-  return temp.join('')
-}
-
-function getDefaultBanner() {
-  return `
-/*!
- * ${pkg.name} v${pkg.version}
- * (c) ${pkg.author}
- * Released under the ${pkg.license} License.
- */`.trim()
-}
-
-function defaultPlugins() {
-  return [
-    babel({
-      runtimeHelpers: true,
-      exclude: ['node_modules/**'],
-    }),
-    node(),
-    cjs(),
-    json(),
-  ];
 }
